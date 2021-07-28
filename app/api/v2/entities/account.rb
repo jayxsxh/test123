openware/peatio

Feature: Add wallet retired status

app/api/v2/entities/account.rb
@@ -41,7 +41,7 @@ module API
             type: String
           }
         ) do |account, options|
-          wallet = Wallet.deposit_wallet(account.currency_id)
+          wallet = Wallet.active_deposit_wallet(account.currency_id)
           ::PaymentAddress.find_by(wallet: wallet, member: options[:current_user], remote: false)
         end
       end

jayxsxh/test123

app/api/v2/entities/account.rb
@@ -42,7 +42,7 @@ module API
             type: String
           }
         ) do |account, options|
-          deposit_wallets = Wallet.deposit_wallets(account.currency_id)
+          deposit_wallets = Wallet.active_deposit_wallets(account.currency_id)
           ::PaymentAddress.where(wallet: deposit_wallets, member: options[:current_user], remote: false)
         end
       end