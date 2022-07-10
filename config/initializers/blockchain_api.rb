86db8744aecace5493b4c2ffcffa2cc22ca61073
Saka <62377315+MonkeyD-Core@users.noreply.github.com>
2022-07-10 17:55:44 +0700

    Update blockchain_api.rb

--- config/initializers/blockchain_api.rb
+++ config/initializers/blockchain_api.rb
@@ -1,4 +1,5 @@
 Peatio::Blockchain.registry[:bitcoin] = Bitcoin::Blockchain
+Peatio::Blockchain.registry[:tyzen] = Tyzen::Blockchain
 Peatio::Blockchain.registry[:geth] = Ethereum::Eth::Blockchain
 Peatio::Blockchain.registry[:parity] = Ethereum::Eth::Blockchain
 Peatio::Blockchain.registry[:"geth-bsc"] = Ethereum::Bsc::Blockchain
